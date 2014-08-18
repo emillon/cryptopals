74920b35b8669c37b66724eb6811f208e79995a0
Etienne Millon <me@emillon.org>
2014-08-18 15:36:18 +0200

    opt xorBuffer

--- XOR.hs
+++ XOR.hs
@@ -6,4 +6,4 @@ import qualified Data.ByteString as B
 
 xorBuffer :: B.ByteString -> B.ByteString -> B.ByteString
 xorBuffer a b =
-    B.pack $ B.zipWith xor a b
+    snd $ B.mapAccumL (\ i w -> (i+1, w `xor` (B.index b i))) 0 a
