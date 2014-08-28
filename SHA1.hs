3ef43bbd3aac2c59ad7a49c487603c30bf224211
Etienne Millon <me@emillon.org>
2014-08-28 16:36:15 +0200

    more test cases

--- SHA1.hs
+++ SHA1.hs
@@ -93,6 +93,14 @@ sha1Tests :: Test
 sha1Tests =
     "SHA1" ~: map (uncurry tc)
         [ ("", "da39a3ee 5e6b4b0d 3255bfef 95601890 afd80709")
+        , ("abc", "a9993e36 4706816a ba3e2571 7850c26c 9cd0d89d")
+        , ( "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
+          , "84983e44 1c3bd26e baae4aa1 f95129e5 e54670f1"
+          )
+        , ( "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"
+          , "a49b2446 a02c645b f419f995 b6709125 3a04a259"
+          )
+        , (replicate 1000000 'a', "34aa973c d4c4daa4 f61eeb2b dbad2731 6534016f")
         ]
     where
         tc input spec =
