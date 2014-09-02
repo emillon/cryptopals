c787cd39957b0b706e4b07179d967236d50163cd
Etienne Millon <me@emillon.org>
2014-09-02 09:39:00 +0200

    make it possible to run just 1 test

--- cryptopals.hs
+++ cryptopals.hs
@@ -560,32 +560,34 @@ main = do
             checkMTProps
             checkMiscProps
             checkDigestProps
-        _ -> void $ runTestTT $ TestList
-            [ chall01
-            , chall02
-            , chall03
-            , chall05
-            , chall06
-            , aesTests
-            , chall07
-            , chall08
-            , chall09
-            , chall10
-            , chall11
-            , chall12
-            , chall13
-            , chall15
-            , chall16
-            , chall17
-            , chall18
-            , chall19
-            , chall20
-            , mtTests
-            , chall22
-            , chall23
-            , chall25
-            , chall27
-            , sha1Tests
-            , chall29
-            , md4Tests
-            ]
+        [n] -> void $ runTestTT $ fromJust $ lookup n tests
+        _ -> void $ runTestTT $ TestList $ map snd tests
+        where
+            tests = [ ("1", chall01)
+                    , ("2", chall02)
+                    , ("3", chall03)
+                    , ("5", chall05)
+                    , ("6", chall06)
+                    , ("aes", aesTests)
+                    , ("7", chall07)
+                    , ("8", chall08)
+                    , ("9", chall09)
+                    , ("10", chall10)
+                    , ("11", chall11)
+                    , ("12", chall12)
+                    , ("13", chall13)
+                    , ("15", chall15)
+                    , ("16", chall16)
+                    , ("17", chall17)
+                    , ("18", chall18)
+                    , ("19", chall19)
+                    , ("20", chall20)
+                    , ("mt", mtTests)
+                    , ("22", chall22)
+                    , ("23", chall23)
+                    , ("25", chall25)
+                    , ("27", chall27)
+                    , ("sha1", sha1Tests)
+                    , ("29", chall29)
+                    , ("md4", md4Tests)
+                    ]
